39352b0bb6bc71a54f39b5afaf6c38b972f0fb8c
vishal-049 <vishal.ramesh004@gmail.com>
2026-02-09 11:42:50 +0530

    Add files via upload

---
+++ prot2-3.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    float a=1.0;
+    printf("%.2f",a);
+
+    return 0;
+}
+
---
+++ prot2-4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    char a='a';
+    printf("%c",a);
+
+    return 0;
+}
+
